b1283e426e9d6d70c936324622bbbc2cd4d4582e
bullet-ant <aman.kr.2129@gmail.com>
2024-08-30 22:56:19 +0530

    adds tippy.js for better tooltips

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,49 @@
 /*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
-*/
+ * Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+ * Copyright 2013-2023 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+ */
 //
 // Scripts
-// 
+//
 
-window.addEventListener('DOMContentLoaded', event => {
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const sideNav = document.body.querySelector('#sideNav');
-    if (sideNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#sideNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Activate Bootstrap scrollspy on the main nav element
+  const sideNav = document.body.querySelector("#sideNav");
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#sideNav",
+      rootMargin: "0px 0px -40%",
     });
+  }
 
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
 });
 
-const checkbox = document.querySelector('input[type="checkbox"]');
+// const checkbox = document.querySelector('input[type="checkbox"]');
 
-checkbox.addEventListener("change", () => {
-	document.body.classList.toggle("dark-body");
+// checkbox.addEventListener("change", () => {
+//   document.body.classList.toggle("dark-body");
+// });
+
+// Tippy.js tooltips
+document.addEventListener("DOMContentLoaded", function () {
+  tippy(".dev-icons > li", {
+    content: (reference) => reference.getAttribute("title"),
+    placement: "top",
+    animation: "shift-away",
+    duration: [500, 200],
+    theme: "charcoal",
+  });
 });
